5888c761ad3dabc79f9fb06112bbd5e8ed514ebd
Jeff Passetti <jpassetti@gmail.com>
2023-10-31 18:25:13 -0400

    added car images

--- components/Swatches.js
+++ components/Swatches.js
@@ -8,12 +8,11 @@ const Swatches = ({
         {data.map((item) => {
             return <li 
                 className={styles.swatch__item}
-                style={{ backgroundColor: item }}
+                style={{ backgroundColor: item.hex }}
                 onClick={() => {
                     changeHandler(item);
                 }}
             >
-                {item}
             </li>
         })}
     </ul>
