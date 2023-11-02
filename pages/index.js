a0ec9af8c1debc576fabdaf7a9aa3d231750cda8
Jeff Passetti <jpassetti@gmail.com>
2023-11-02 18:22:19 -0400

    Added layout component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Button from '../components/Button';
 import CarColorPicker from '../components/CarColorPicker';
+import Layout from '../components/Layout';
 
 const Homepage = () => {
-  return <div>
+  return <Layout>
     <CarColorPicker />
-
-
     <h1>Homepage</h1>
     <Button 
       label="Register now"
@@ -16,6 +15,6 @@ const Homepage = () => {
       type="secondary"
     />
     <Button label="Learn more" />
-  </div>
+  </Layout>
 }
 export default Homepage;
