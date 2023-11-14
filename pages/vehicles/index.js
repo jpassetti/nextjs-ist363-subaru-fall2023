ad36b3a82fda304fb7a8b3a3e23c6abf4c4d7de7
Jeff Passetti <jpassetti@gmail.com>
2023-11-14 18:35:07 -0500

    added graphql to /vehicles/index.js

--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -4,24 +4,21 @@ import Link from 'next/link';
 import { getAllVehicles } from '../../lib/api';
 
 export async function getStaticProps() {
-    const vehicles = getAllVehicles();
-
+    const vehicles = await getAllVehicles();
     return {
         props: {
             vehicles
         }
     }
 }
-
 const VehiclesPage = ({ vehicles }) => {
     return <Layout>
         <h1>Vehicles</h1>
         <ul>
             {vehicles.map((vehicle, index) => {
-                const { model, price, slug } = vehicle;
+                const { title, slug } = vehicle.node;
                 return <li key={index}>
-                    <h3>{model}</h3>
-                    <h4>${price}</h4>
+                    <h3>{title}</h3>
                     <p>
                         <Link href={`/vehicles/${slug}`}>Learn more</Link>
                     </p>
