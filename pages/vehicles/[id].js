de7b5ad87e0168e0208fd8f19ff94b4bf5aec060
Jeff Passetti <jpassetti@gmail.com>
2023-11-16 18:23:32 -0500

    added [id].js

--- pages/vehicles/[id].js
+++ pages/vehicles/[id].js
@@ -4,7 +4,7 @@ import { getVehicleBySlug, getAllVehicleSlugs } from '../../lib/api';
 // WATERFALL
 // 1. getStaticPaths
 export async function getStaticPaths() {
-    const vehicles = getAllVehicleSlugs();
+    const vehicles = await getAllVehicleSlugs();
     const paths = vehicles.map((vehicle) => {
         const { slug } = vehicle.node;
         return {
@@ -20,7 +20,7 @@ export async function getStaticPaths() {
 }
 // 2. getStaticProps
 export async function getStaticProps({ params }) {
-    const vehicleData = getVehicleBySlug(params.id);
+    const vehicleData = await getVehicleBySlug(params.id);
     return {
         props : {
             vehicleData
@@ -29,10 +29,9 @@ export async function getStaticProps({ params }) {
 }
 // 3. page component
 const SingleVehiclePage = ({ vehicleData }) => {
-    const { model, price } = vehicleData;
+    const { title, slug } = vehicleData;
     return <Layout>
-        <h1>{model}</h1>
-        <h2>${price}</h2>
+        <h1>{title}</h1>
     </Layout>
 }
 export default SingleVehiclePage;
