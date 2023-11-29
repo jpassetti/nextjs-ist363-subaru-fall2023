b51251df091492a354d8bec058a49c96f191dabb
Jeff Passetti <jpassetti@gmail.com>
2023-11-29 13:52:30 -0500

    single car template

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'ist363-fall-2023-subaru.local'
+        },
+        {
+          protocol: 'https',
+          hostname: 'subaru.ist363.xyz'
+        },
+      ],
+    }
+  } 
