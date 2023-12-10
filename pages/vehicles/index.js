c927ca4834f477506c95386680e7640071d0ad4c
Jeff Passetti <jpassetti@gmail.com>
2023-12-09 22:54:30 -0500

    code after videos for vehicles/index.js

--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,28 +1,61 @@
+import { useState } from 'react';
+
 import Container from '../../components/Container';
+import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
 import Image from 'next/image';
 import Layout from '../../components/Layout';
 import Link from 'next/link';
 
-import { getAllVehicles } from '../../lib/api';
+import { getAllVehicles, getVehicleTypes } from '../../lib/api';
 
 export async function getStaticProps() {
     const vehicles = await getAllVehicles();
+    const vehicleTypes = await getVehicleTypes();
+
+    vehicleTypes.unshift({
+        "node": {
+          "name": "All",
+          "slug": "all"
+        }
+      },);
+
     return {
         props: {
-            vehicles
+            vehicles,
+            vehicleTypes
         }
     }
 }
-const VehiclesPage = ({ vehicles }) => {
+const VehiclesPage = ({ vehicles, vehicleTypes }) => {
+    // add "all" to vehicleTypes
+    const [activeVehicleType, setActiveVehicleType] = useState('all');
+
+    // filter vehicles by activeVehicleType
+    const filteredVehicles = activeVehicleType === 'all' ? 
+        vehicles 
+        : 
+        vehicles.filter((vehicle) => {
+            const { vehicleTypes } = vehicle.node;
+            const vehicleTypeSlugs = vehicleTypes.edges.map((vehicleType) => {
+                return vehicleType.node.slug;
+            });
+            return vehicleTypeSlugs.includes(activeVehicleType);
+     });
+
     return <Layout>
         <h1>Vehicles</h1>
         <Container>
+            <FilterBar 
+                items={vehicleTypes} 
+                activeItem={activeVehicleType} 
+                setActiveItem={setActiveVehicleType}
+            />
             <Grid>
-                {vehicles.map((vehicle, index) => {
+                {filteredVehicles.map((vehicle, index) => {
                     const { title, slug, vehicleInformation } = vehicle.node;
                     const { trimLevels } = vehicleInformation;
-                    return <li key={index}>
+                    return <article key={index}>
                         {trimLevels && trimLevels[0].images.thumbnail && 
                             <Image 
                                 src={trimLevels[0].images.thumbnail.node.sourceUrl}
@@ -35,7 +68,7 @@ const VehiclesPage = ({ vehicles }) => {
                         <p>
                             <Link href={`/vehicles/${slug}`}>Learn more</Link>
                         </p>
-                    </li>
+                    </article>
                 })}
             </Grid>
         </Container>
